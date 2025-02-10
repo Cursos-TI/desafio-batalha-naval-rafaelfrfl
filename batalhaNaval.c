83123b3f3bbe36b565113a614fc7ba23dc83e1c2
rafaelfrfl <rafael2017fernando@gmail.com>
2025-02-10 01:58:31 -0300

    Update batalhaNaval.c

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -4,7 +4,8 @@
 // Este código inicial serve como base para o desenvolvimento do sistema de Batalha Naval.
 // Siga os comentários para implementar cada parte do desafio.
 
-int main() {
+tabuleiro([4][4]); {
+tabuleiro([6][6];}    
     // Nível Novato - Posicionamento dos Navios
     // Sugestão: Declare uma matriz bidimensional para representar o tabuleiro (Ex: int tabuleiro[5][5];).
     // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
@@ -38,3 +39,4 @@ int main() {
 
     return 0;
 }
+
